be0ab0037c13ef91ec5e8c039a3d5e75a2f7fcaa
Kev <kevinraffetseder@gmail.com>
2024-02-12 20:28:48 +0100

    titlescreen visuals added ++

--- MMP2a/Manager/PhysicsManager.cpp
+++ MMP2a/Manager/PhysicsManager.cpp
@@ -173,54 +173,54 @@ void PhysicsManager::collisionResolve()
 {
     for (auto& man : manifolds)
     {
-            std::shared_ptr<GameObject> go1 = man->body1;
-            std::shared_ptr<GameObject> go2 = man->body2;
-            sf::Vector2f separationVector = man->normal * man->penetration;
-            // Collisions against Players are not being detected
-            if (go1->getId().find("Player") != std::string::npos && go2->getId().find("Player") != std::string::npos)
-            {
-                continue;
-            }
+        std::shared_ptr<GameObject> go1 = man->body1;
+        std::shared_ptr<GameObject> go2 = man->body2;
+        sf::Vector2f separationVector = man->normal * man->penetration;
+        // Collisions against Players are not being detected
+        if (go1->getId().find("Player") != std::string::npos && go2->getId().find("Player") != std::string::npos)
+        {
+            continue;
+        }
 
-            if (go1->getComponentsOfType<RectCollisionCP>().at(0)->isTrigger() || go2->getComponentsOfType<RectCollisionCP>().at(0)->isTrigger())
+        if (go1->getComponentsOfType<RectCollisionCP>().at(0)->isTrigger() || go2->getComponentsOfType<RectCollisionCP>().at(0)->isTrigger())
+        {
+            if (go1->getComponentsOfType<RigidBodyCP>().size() != 0  && go1->getId().find("Boundary") == std::string::npos)
             {
-                if (go1->getComponentsOfType<RigidBodyCP>().size() != 0  && go1->getId().find("Boundary") == std::string::npos)
+                if (go2->getId().find("Skeleton") != std::string::npos)
                 {
-                    if (go2->getId().find("Skeleton") != std::string::npos)
-                    {
-                        // Send Notification to Rigid Body of go1
-                        //go1->getComponentsOfType<RigidBodyCP>().at(0)->onCollision(go2);
-                    }
-                    else
-                    {
-                        sf::Vector2f newPos = go1->getComponentsOfType<RigidBodyCP>().at(0)->getPos() + separationVector;
-                        go1->getComponentsOfType<RigidBodyCP>().at(0)->setPos(newPos);
-                        go1->getComponentsOfType<TransformationCP>().at(0)->setPosition(newPos);
-                        go1->getComponentsOfType<RigidBodyCP>().at(0)->onCollision(go2);
-                    }
+                    // Send Notification to Rigid Body of go1
+                    //go1->getComponentsOfType<RigidBodyCP>().at(0)->onCollision(go2);
                 }
-                else if(go2->getComponentsOfType<RigidBodyCP>().size() != 0 && go2->getId().find("Boundary") == std::string::npos)
+                else
                 {
-                    if (go2->getId().find("Skeleton") != std::string::npos)
-                    {
-                        // Send Notification to Rigid Body of go2
-                        //go2->getComponentsOfType<RigidBodyCP>().at(0)->onCollision(go1);
-                    }
-                    else
-                    {
-                        sf::Vector2f newPos = go2->getComponentsOfType<RigidBodyCP>().at(0)->getPos() + separationVector;
-                        go2->getComponentsOfType<RigidBodyCP>().at(0)->setPos(newPos);
-                        go2->getComponentsOfType<TransformationCP>().at(0)->setPosition(newPos);
-                        go2->getComponentsOfType<RigidBodyCP>().at(0)->onCollision(go1);
-                    }
+                    sf::Vector2f newPos = go1->getComponentsOfType<RigidBodyCP>().at(0)->getPos() + separationVector;
+                    go1->getComponentsOfType<RigidBodyCP>().at(0)->setPos(newPos);
+                    go1->getComponentsOfType<TransformationCP>().at(0)->setPosition(newPos);
+                    go1->getComponentsOfType<RigidBodyCP>().at(0)->onCollision(go2);
                 }
             }
+            else if(go2->getComponentsOfType<RigidBodyCP>().size() != 0 && go2->getId().find("Boundary") == std::string::npos)
+            {
+                if (go2->getId().find("Skeleton") != std::string::npos)
+                {
+                    // Send Notification to Rigid Body of go2
+                    //go2->getComponentsOfType<RigidBodyCP>().at(0)->onCollision(go1);
+                }
+                else
+                {
+                    sf::Vector2f newPos = go2->getComponentsOfType<RigidBodyCP>().at(0)->getPos() + separationVector;
+                    go2->getComponentsOfType<RigidBodyCP>().at(0)->setPos(newPos);
+                    go2->getComponentsOfType<TransformationCP>().at(0)->setPosition(newPos);
+                    go2->getComponentsOfType<RigidBodyCP>().at(0)->onCollision(go1);
+                }
+            }
+        }
     }
 }
 
 void PhysicsManager::update(std::vector<std::shared_ptr<GameObject>> gameObjects, float deltaTime)
 {
-    const float step = 0.02f;
+    const float step = 0.001f;
     accumulator += deltaTime;
 
     while (accumulator > step)
